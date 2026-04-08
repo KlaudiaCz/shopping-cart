f3a9242cf9d30543a0f943d18f26ffd05170a280
Klaudia Czetyrow <czetyrow@gmail.com>
2026-04-08 22:35:47 +0200

    Added sort selector. Updated db.json.

--- src/App.jsx
+++ src/App.jsx
@@ -4,25 +4,39 @@ import ProductList from "./components/ProductList";
 
 const App = () => {
   const [search, setSearch] = useState("");
-
+  const [sort, setSort] = useState("");
   return (
     <>
       <Header />
       <div className="min-h-screen bg-gray-100 p-6">
         <h1 className="text-3xl font-bold mb-6">🛒 Product Catalog</h1>
         {/* Search Bar and Filters */}
-        <div className="">
-          <div>
+        <div className="flex gap-4">
+          {/* Search Bar */}
+          <div className="flex-1">
             <input
-              className="p-2 border w-full rounded-xl mb-4 border-gray-500 placeholder-gray-500/50 text-black"
+              className="p-2 border max-w-lg w-full rounded-xl mb-4 border-gray-500 placeholder-gray-500/50 text-black"
               type="text"
               onChange={(e) => setSearch(e.target.value)}
               value={search}
               placeholder="Search for products..."
             />
           </div>
+          {/* SortBy */}
+          <div>
+            <label htmlFor="sort">Sort By: </label>
+            <select value={sort} onChange={(e) => setSort(e.target.value)} id="sort" className="p-2 border rounded-xl">
+              <option value="">---</option>
+              <option value="price-asc">Price ↑</option>
+              <option value="price-desc">Price ↓</option>
+              <option value="name-asc">Name A-Z</option>
+              <option value="name-desc">Name Z-A</option>
+              <option value="category-asc">Category A-Z</option>
+              <option value="category-desc">Category Z-A</option>
+            </select>
+          </div>
         </div>
-        <ProductList search={search}/>
+        <ProductList search={search} sort={sort} />
       </div>
     </>
   );
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -2,22 +2,45 @@ import ProductCard from "./ProductCard";
 import { useContext } from "react";
 import { ProductContext } from "../context/ProductContext";
 
-const ProductList = ({ search }) => {
+const ProductList = ({ search, sort }) => {
   const { products, loading, error } = useContext(ProductContext); // Access products, loading, and error from ProductContext
 
   const filteredProducts = products.filter((product) =>
     product.name.toLowerCase().includes(search.toLowerCase()),
   );
 
+  const sortedProducts = [...filteredProducts].sort((a, b) => {
+    switch (sort) {
+      case "price-asc":
+        return a.price - b.price;
+      case "price-desc":
+        return b.price - a.price;
+      case "name-asc":
+        return a.name.localeCompare(b.name);
+      case "name-desc":
+        return b.name.localeCompare(a.name);
+      case "category-asc":
+        return a.category.localeCompare(b.category);
+      case "category-desc":
+        return b.category.localeCompare(a.category);
+    }
+
+    // if (sort === "price-asc") return a.price - b.price;
+    // if (sort === "price-desc") return b.price - a.price;
+    // if (sort === "name-asc") return a.name.localeCompare(b.name);
+    // if (sort === "name-desc") return b.name.localeCompare(a.name);
+    return 0;
+  });
+
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
       {loading && <p>Loading...</p>}
       {error && <div className="text-red-500">❌ {error}</div>}
-      {filteredProducts.map((product) => (
+      {sortedProducts.map((product) => (
         <ProductCard key={product.id} product={product} />
       ))}
 
-      {filteredProducts.length === 0 && !loading && <p>No products found 😢</p>}
+      {sortedProducts.length === 0 && !loading && <p>No products found 😢</p>}
     </div>
   );
 };
