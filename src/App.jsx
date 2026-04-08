350b90ae739345e231837d35fa6f567423130156
Klaudia Czetyrow <czetyrow@gmail.com>
2026-04-08 13:57:42 +0200

    Added search bar with filter

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
+import { useState } from "react";
 import Header from "./components/Header";
 import ProductList from "./components/ProductList";
 
 const App = () => {
+  const [search, setSearch] = useState("");
+
   return (
     <>
       <Header />
       <div className="min-h-screen bg-gray-100 p-6">
         <h1 className="text-3xl font-bold mb-6">🛒 Product Catalog</h1>
-
-        <ProductList />
+        {/* Search Bar and Filters */}
+        <div className="">
+          <div>
+            <input
+              className="p-2 border w-full rounded-xl mb-4 border-gray-500 placeholder-gray-500/50 text-black"
+              type="text"
+              onChange={(e) => setSearch(e.target.value)}
+              value={search}
+              placeholder="Search for products..."
+            />
+          </div>
+        </div>
+        <ProductList search={search}/>
       </div>
     </>
   );
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -2,16 +2,22 @@ import ProductCard from "./ProductCard";
 import { useContext } from "react";
 import { ProductContext } from "../context/ProductContext";
 
-const ProductList = () => {
+const ProductList = ({ search }) => {
   const { products, loading, error } = useContext(ProductContext); // Access products, loading, and error from ProductContext
+
+  const filteredProducts = products.filter((product) =>
+    product.name.toLowerCase().includes(search.toLowerCase()),
+  );
 
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
       {loading && <p>Loading...</p>}
       {error && <div className="text-red-500">❌ {error}</div>}
-      {products.map((product) => (
+      {filteredProducts.map((product) => (
         <ProductCard key={product.id} product={product} />
       ))}
+
+      {filteredProducts.length === 0 && !loading && <p>No products found 😢</p>}
     </div>
   );
 };
